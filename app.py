ba9ebc14be3e4ef0e491552b2cf2dc6b0c8475d8
Cholan-kinnera <imcholan.kinnera@gmail.com>
2026-03-07 20:30:29 +0530

    Added final changes

--- app.py
+++ app.py
@@ -30,7 +30,10 @@ limiter = Limiter(
 
 limiter.init_app(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///database.db"
+ )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 
